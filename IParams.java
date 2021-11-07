13fa10d3ae714a79de0dc8b16bdf8e618aa46aef
moji <224292262@qq.com>
2021-11-07 13:25:48 +0800

    IParams接口

---
+++ IParams.java
@@ -0,0 +1,13 @@
+package git;
+import java.util.*;
+public interface IParams {
+    public ArrayList<Integer> getPlanParking =new ArrayList<Integer>(100);
+    // 获取停车序列，例如 [1 2 2 3] 表示 依次停一辆大车，中车，中车，小车
+    public ArrayList<Integer> getPlanParking();
+    // 获取大车位
+   public int getBig();
+    // 获取中车位
+    public int getMedium();
+    // 获取小车位
+    public int getSmall();
+}
